7519e3ef7acb5bb60eb0d0d81b35720c7c8ffa35
Adnan-Dev <adnanqaidjoher8962@gmail.com>
2024-07-03 17:24:00 +0530

    Updates

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 
 module.exports = {
-  port: process.env.PORT || 3000,
-  baseUrl: process.env.BASE_URL,
-  connectionUrl: process.env.CONNECTION_URL,
-  jwtSecret: process.env.JWT_SECRET,
-  accessTokenExpiration: process.env.ACCESS_TOKEN_EXPIRATION,
-  refreshTokenExpiration: process.env.REFRESH_TOKEN_EXPIRATION,
+  port: process.env.PORT || 4000,
+  baseUrl: process.env.BASE_URL || "http://192.168.31.238:4000/",
+  connectionUrl: process.env.CONNECTION_URL || "mongodb://localhost:27017/test",
+  jwtSecret: process.env.JWT_SECRET || "ASDhffcSAEWvbc",
+  accessTokenExpiration: process.env.ACCESS_TOKEN_EXPIRATION || "1d",
+  refreshTokenExpiration: process.env.REFRESH_TOKEN_EXPIRATION || "2d",
   emailCreds: {
     service: "gmail",
     userEmail: "adnanqaidjoher8962@gmail.com",
